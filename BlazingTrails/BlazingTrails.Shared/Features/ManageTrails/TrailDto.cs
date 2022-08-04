5f94e978146f632fa4074cabc76a464d17346168
Brian Meeker <meeker.brian@gmail.com>
2022-08-04 16:05:51 -0400

    Collect how long it takes to hike the trail with a custom input

--- BlazingTrails/BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails/BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -29,6 +29,8 @@ public class TrailValidator : AbstractValidator<TrailDto>
             .WithMessage("Please enter a description");
         RuleFor(x => x.Location).NotEmpty()
             .WithMessage("Please enter a location");
+        RuleFor(x => x.TimeInMinutes).GreaterThan(0)
+            .WithMessage("Please enter a time");
         RuleFor(x => x.Length).GreaterThan(0)
             .WithMessage("Please enter a length");
         RuleFor(x => x.Route).NotEmpty()
